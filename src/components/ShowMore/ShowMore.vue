29d538422c943cacbd6472cd0e22abc01eb9ee7c
徐敏 <xumin@xumindeMBP.lan>
2018-08-19 18:30:00 +0800

    :tada: showMore组件 first commit

---
+++ src/components/ShowMore/ShowMore.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="sm-container">
+    <div :ref="`sm-${id}`" class="sm-content">
+      {{ data }}
+    </div>
+    <div v-if="isShowMore" class="sm-btn pointer" @click="showMore">
+      <i v-if="iconFront" :class="['sm-btn-icon', isUnfold ? showLessBtnIcon : showMoreBtnIcon]"></i>
+      <span class="sm-btn-txt">{{ isUnfold ? showLessBtnTxt : showMoreBtnTxt }}</span>
+      <i v-if="!iconFront" :class="['sm-btn-icon', isUnfold ? showLessBtnIcon : showMoreBtnIcon]"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'show-more',
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    data: {
+      type: String,
+      default: ''
+    },
+    showContentHeight: {
+      type: String,
+      default: '80'
+    },
+    iconFront: {
+      type: Boolean,
+      default: false
+    },
+    showMoreBtnTxt: {
+      type: String,
+      default: '显示更多'
+    },
+    showMoreBtnIcon: {
+      type: String,
+      default: 'iconfont icon-jiantouxia'
+    },
+    showLessBtnTxt: {
+      type: String,
+      default: '收起'
+    },
+    showLessBtnIcon: {
+      type: String,
+      default: 'iconfont icon-jiantoushang'
+    }
+  },
+  data () {
+    return {
+      isShowMore: false,
+      isUnfold: false
+    }
+  },
+  mounted () {
+    const smContentEle = this.$refs[`sm-${this.id}`] || document.querySelector('.sm-content')
+    if (smContentEle.clientHeight > this.showContentHeight) {
+      this.isShowMore = true
+      smContentEle.style.height = this.showContentHeight + 'px'
+    } else {
+      this.isShowMore = false
+      smContentEle.style.height = 'auto'
+    }
+  },
+  methods: {
+    showMore () {
+      const smContentEle = this.$refs[`sm-${this.id}`] || document.querySelector('.sm-content')
+      if (this.isUnfold) {
+        this.isUnfold = false
+        smContentEle.style.height = this.showContentHeight + 'px'
+      } else {
+        this.isUnfold = true
+        smContentEle.style.height = 'auto'
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.sm-content{
+  overflow: hidden;
+  white-space: pre-wrap;
+}
+</style>
